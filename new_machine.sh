f3fb8314a9d5c4ece139f963c9aa721449d411b5
Mark Santolucito <mark.santolucito@yale.edu>
2016-02-22 02:10:35 +0900

    i tihnk this shold work

---
+++ new_machine.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install haskell-platform git
+cabal update
+git clone https://github.com/santolucito/scripts.git
+cd scripts
+mv .vimrc ~/.vimrc
+mv .profile ~/.profile
+
+sudo add-apt-repository -y ppa:hvr/ghc
+sudo apt-get update
+sudo apt-get install cabal-install-1.18 ghc-7.8.4 ocaml camlidl
+echo "PATH=\"$HOME/.cabal/bin:/opt/ghc/7.8.4/bin:/opt/cabal/1.18/bin:$PATH\"" ~/.profile
