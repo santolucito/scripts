85863fe3d27b41a89f5ad54399a763ec35c708a9
Mark Santolucito <msantolucuito13@gmail.com>
2016-03-06 22:49:09 +0900

    think i need to say yes

--- new_machine.sh
+++ new_machine.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install haskell-platform git
+y
 cabal update
 git clone https://github.com/santolucito/scripts.git
 cd scripts
@@ -9,4 +10,5 @@ mv .profile ~/.profile
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
 sudo apt-get install cabal-install-1.18 ghc-7.8.4 ocaml camlidl
+y
 echo "PATH=\"$HOME/.cabal/bin:/opt/ghc/7.8.4/bin:/opt/cabal/1.18/bin:$PATH\"" ~/.profile
